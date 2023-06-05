5df92f2eccc32b992f9fedf46f2172127bffb82a
asel <asel@gmail.com>
2023-06-05 09:07:08 -0500

    add prefix url

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/patient',require('./src/app/patients/router'));
-app.use('/room',require('./src/app/rooms/router'));
-app.use('/appointment',require('./src/app/appointments/router'));
+app.use('/dentist/patient',require('./src/app/patients/router'));
+app.use('/dentist/room',require('./src/app/rooms/router'));
+app.use('/dentist/appointment',require('./src/app/appointments/router'));
 
-app.use('/user',require('./src/app/users/router'));
-app.use('/bill',require('./src/app/bills/router'));
-app.use('/store',require('./src/app/stores/router'));
+app.use('/dentist/user',require('./src/app/users/router'));
+app.use('/dentist/bill',require('./src/app/bills/router'));
+app.use('/dentist/store',require('./src/app/stores/router'));
 
 
 app.listen({ port: 3000 }, async () => {
